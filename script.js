9d43a1306e3769312bdb6316015dd9e9e52e7155
Shubham Ashish <78084828+shubhamashish33@users.noreply.github.com>
2021-08-07 21:14:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+getSum = () => {
+  let num1 = parseFloat(document.getElementById('firstNum').value);
+  let num2 = parseFloat(document.getElementById('secondNum').value);
+  document.getElementById('result').innerHTML = num1 + num2
+}
