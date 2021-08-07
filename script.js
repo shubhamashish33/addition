ed39fa5cd3db10301a72a4f613bd3c5417c01472
Shubham Ashish <78084828+shubhamashish33@users.noreply.github.com>
2021-08-07 21:32:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 getSum = () => {
   let num1 = parseFloat(document.getElementById('firstNum').value);
   let num2 = parseFloat(document.getElementById('secondNum').value);
-  document.getElementById('result').innerHTML = num1 + num2
+  if(isNaN(num1) || isNaN(num2))
+  {
+    document.getElementById('result').innerHTML = 'Please Enter Value';
+  }
+  else{
+  document.getElementById('result').innerHTML = num1 + num2;
+  }
 }
