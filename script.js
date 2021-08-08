4fcf3508252d1f8c5d9f909a955355a3e2e5b6a0
Shubham Ashish <78084828+shubhamashish33@users.noreply.github.com>
2021-08-08 22:26:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-getSum = () => {
-  let num1 = parseFloat(document.getElementById('firstNum').value);
-  let num2 = parseFloat(document.getElementById('secondNum').value);
-  if(isNaN(num1) || isNaN(num2))
-  {
-    document.getElementById('result').innerHTML = 'Please Enter Value';
-  }
-  else{
-  document.getElementById('result').innerHTML = num1 + num2;
-  }
+getCalc = (choise) => {
+   let n1 = parseFloat(document.getElementById('firstNum').value);
+   let n2 = parseFloat(document.getElementById('secondNum').value);
+   let r;
+   let c = choise;
+   if (isNaN(n1) || isNaN(n2)) {
+      document.getElementById('result').innerHTML = 'Please Enter Value';
+   }
+   else {
+      switch (c) {
+         case '1':
+            r = n1 + n2;
+            break;
+         case '2':
+            r = n1 - n2;
+            break;
+         case '3':
+            r = n1 * n2;
+            break;
+         case '4':
+            r = n1 / n2;
+            break;
+         default:
+            break;
+      }
+      document.getElementById('result').innerHTML = r;
+   }
+
 }
-// disable develope mode
+
+// disable developer mode
 document.onkeydown = function(e) {
   if(e.keyCode == 123) {
      return false;
@@ -27,3 +46,4 @@ document.onkeydown = function(e) {
      return false;
   }
 }
+
